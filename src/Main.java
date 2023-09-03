8c1b2b8d91a486d0e7828ce7ffdd2f23c55c313c
Iman <imanali.cse@gmail.com>
2023-09-03 12:36:29 +0600

    Folder watcher

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+
+        String currentDir = "C:\\BM\\codehub\\FolderWatcher\\changeFiles";
+        System.out.println("Current dir using System 1:" + currentDir);
+
+        boolean recursive = false;
+        recursive = true;
+
+        // register directory and process its events
+        Path dir = Paths.get(currentDir);
+        new WatchDir(dir, recursive).processEvents();
+
+    }
+}
